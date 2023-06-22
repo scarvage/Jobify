539691a2ec617741b6dece190e388ecc963d6a6e
scarvage <mojha001@gmail.com>
2023-06-22 20:13:25 +0530

    database connected

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+//middleware
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import connectDB from './db/connect.js'
+
+app.get("/",(req,res)=>{
+    throw new Error('error')
+    res.send("Welcome");
+})
+
+app.use(notFoundMiddleware )
+app.use(errorHandlerMiddleware)
+const port = process.env.PORT || 5000
+
+
+
+const start = async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, ()=>{
+            console.log(`Server is Running on port ${port}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
