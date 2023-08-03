3bfa480a1994ff8b88a64dc93ac0f7674e465b9f
scarvage <mojha001@gmail.com>
2023-08-03 11:46:46 +0530

    finally done

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,5 +1,4 @@
-const notFoundMiddleware = (req,res)=>{
-    res.status(404).send('Route Does not exist')
-}
+const notFoundMiddleware = (req, res) =>
+  res.status(404).send('Route does not exist')
 
 export default notFoundMiddleware
